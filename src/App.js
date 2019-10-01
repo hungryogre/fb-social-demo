ffca8d4b207d62c0b52ed10a9faa6f4ab4aabd13
Marcel Guerin <20742004+hungryogre@users.noreply.github.com>
2019-10-01 11:18:09 -0500

    add OAuth wrapper

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Amplify from 'aws-amplify';
+import { withOAuth } from 'aws-amplify-react';
+import aws_exports from './aws-exports';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+Amplify.configure(aws_exports);
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
         </a>
-      </header>
-    </div>
-  );
+                    <button onClick={this.props.OAuthSignIn}>
+                        Sign in with Facebook
+        </button>
+                </header>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default withOAuth(App);
